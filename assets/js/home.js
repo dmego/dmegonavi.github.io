815a7ee16fb06d2614ac26bce0e76a8ff2462fb9
dmego <zengkai12138@outlook.com>
2017-10-06 03:47:40 +0800

    简精简代码

--- assets/js/home.js
+++ assets/js/home.js
@@ -18,62 +18,10 @@ $('#page-loading-progress').progress({
 });
 
 // GitHub
-function updateGitHub(repository) {
-    $('#github-watch img').attr('src', 'https://img.shields.io/github/watchers/' + repository + '.svg?style=social&label=Watch');
-    $('#github-star img').attr('src', 'https://img.shields.io/github/stars/' + repository + '.svg?style=social&label=Star');
-    $('#github-fork img').attr('src', 'https://img.shields.io/github/forks/' + repository + '.svg?style=social&label=Fork');
-};
-var repository = $('meta[name=repository]').attr('content');
-updateGitHub(repository);
-setInterval(function () {
-    updateGitHub(repository);
-}, 15000);
 
 // version
-moment.locale('zh-cn');
-function updateVersion(timestamp) {
-    $('#version img').attr('src', 'https://img.shields.io/badge/%E6%9B%B4%E6%96%B0%E4%BA%8E-' + encodeURIComponent(moment(timestamp).fromNow()) + '-brightgreen.svg');
-};
-var updateAt = $('meta[name=updated_at]').attr('content');
-updateVersion(updateAt);
-setInterval(function () {
-    updateVersion(updateAt);
-}, 15000);
 
 // visit
-function updateVisit(updateProgressBar) {
-    $.getJSON(analyticsAPI.url, {
-        'module': 'API',
-        'method': 'VisitsSummary.getUniqueVisitors',
-        'idSite': analyticsAPI.id,
-        'period': 'day',
-        'date': 'today',
-        'format': 'JSON',
-        'token_auth': analyticsAPI.token
-    }, function (data) {
-        $('#today-visitors img').attr('src', 'https://img.shields.io/badge/%E4%BB%8A%E6%97%A5%E8%AE%BF%E5%AE%A2-' + encodeURIComponent(data.value) + '-brightgreen.svg');
-        if (updateProgressBar) {
-            $('#page-loading-progress').progress('increment');
-        };
-    });
-    $.getJSON(analyticsAPI.url, {
-        'module': 'API',
-        'method': 'Live.getCounters',
-        'idSite': analyticsAPI.id,
-        'lastMinutes': '30',
-        'format': 'JSON',
-        'token_auth': analyticsAPI.token
-    }, function (data) {
-        $('#live-visitors img').attr('src', 'https://img.shields.io/badge/%E5%BD%93%E5%89%8D%E5%9C%A8%E7%BA%BF-' + encodeURIComponent(data[0].visitors) + '-brightgreen.svg');
-        if (updateProgressBar) {
-            $('#page-loading-progress').progress('increment');
-        };
-    });
-};
-updateVisit(true);
-setInterval(function () {
-    updateVisit(false);
-}, 15000);
 
 // search
 $('#search-services').dropdown();
